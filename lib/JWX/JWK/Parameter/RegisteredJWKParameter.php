9c8c21017ca76fca961a296aed760f60d3cb01be
Joni Eskelinen <joni@mobie.fi>
2016-04-11 16:31:14 +0300

    Add constants for remaining registered parameters

--- lib/JWX/JWK/Parameter/RegisteredJWKParameter.php
+++ lib/JWX/JWK/Parameter/RegisteredJWKParameter.php
@@ -3,6 +3,11 @@
 namespace JWX\JWK\Parameter;
 
 
+/**
+ * IANA registered JSON Web Key Parameters
+ *
+ * @link http://www.iana.org/assignments/jose/jose.xhtml#web-key-parameters
+ */
 abstract class RegisteredJWKParameter extends JWKParameter
 {
 	const PARAM_KEY_TYPE = "kty";
@@ -10,6 +15,24 @@ abstract class RegisteredJWKParameter extends JWKParameter
 	const PARAM_KEY_OPERATIONS = "key_ops";
 	const PARAM_ALGORITHM = "alg";
 	const PARAM_KEY_ID = "kid";
+	const PARAM_X509_URL = "x5u";
+	const PARAM_X509_CERTIFICATE_CHAIN = "x5c";
+	const PARAM_X509_CERTIFICATE_SHA1_THUMBPRINT = "x5t";
+	const PARAM_X509_CERTIFICATE_SHA256_THUMBPRINT = "x5t#S256";
+	const PARAM_CURVE = "crv";
+	const PARAM_X_COORDINATE = "x";
+	const PARAM_Y_COORDINATE = "y";
+	const PARAM_ECC_PRIVATE_KEY = "d";
+	const PARAM_MODULUS = "n";
+	const PARAM_EXPONENT = "e";
+	const PARAM_PRIVATE_EXPONENT = "d";
+	const PARAM_FIRST_PRIME_FACTOR = "p";
+	const PARAM_SECOND_PRIME_FACTOR = "q";
+	const PARAM_FIRST_FACTOR_CRT_EXPONENT = "dp";
+	const PARAM_SECOND_FACTOR_CRT_EXPONENT = "dq";
+	const PARAM_FIRST_CRT_COEFFICIENT = "qi";
+	const PARAM_OTHER_PRIMES_INFO = "oth";
+	const PARAM_KEY_VALUE = "k";
 	
 	/**
 	 * Mapping from registered JWK parameter name to class name
@@ -22,7 +45,8 @@ abstract class RegisteredJWKParameter extends JWKParameter
 		self::PARAM_PUBLIC_KEY_USE => PublicKeyUseParameter::class,
 		self::PARAM_KEY_OPERATIONS => KeyOperationsParameter::class,
 		self::PARAM_ALGORITHM => AlgorithmParameter::class,
-		self::PARAM_KEY_ID => KeyIDParameter::class
+		self::PARAM_KEY_ID => KeyIDParameter::class,
+		self::PARAM_KEY_VALUE => KeyValueParameter::class
 	);	// @formatter:on
 	
 
