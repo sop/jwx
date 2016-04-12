d361b653f714b62d10228ef9e89991703ceaaee8
Joni Eskelinen <joni@mobie.fi>
2016-04-12 13:12:07 +0300

    Add comments

--- lib/JWX/JWT/Parameter/JWTParameter.php
+++ lib/JWX/JWT/Parameter/JWTParameter.php
@@ -5,15 +5,40 @@ namespace JWX\JWT\Parameter;
 
 class JWTParameter
 {
+	/**
+	 * Parameter name
+	 *
+	 * @var string $_name
+	 */
 	protected $_name;
 	
+	/**
+	 * Parameter value
+	 *
+	 * @var mixed $_value
+	 */
 	protected $_value;
 	
+	/**
+	 * Constructor
+	 *
+	 * @param string $name Parameter name
+	 * @param mixed $value Parameter value
+	 */
 	public function __construct($name, $value) {
 		$this->_name = $name;
 		$this->_value = $value;
 	}
 	
+	/**
+	 * Initialize from name and value.
+	 *
+	 * Returns parameter specific object if one is implemented.
+	 *
+	 * @param string $name Parameter name
+	 * @param mixed $value Parameter value
+	 * @return self
+	 */
 	public static function fromNameAndValue($name, $value) {
 		switch ($name) {
 		case RegisteredParameter::NAME_TYPE:
@@ -26,10 +51,20 @@ class JWTParameter
 		return new JWTParameter($name, $value);
 	}
 	
+	/**
+	 * Get parameter name
+	 *
+	 * @return string
+	 */
 	public function name() {
 		return $this->_name;
 	}
 	
+	/**
+	 * Get parameter value
+	 *
+	 * @return mixed
+	 */
 	public function value() {
 		return $this->_value;
 	}
