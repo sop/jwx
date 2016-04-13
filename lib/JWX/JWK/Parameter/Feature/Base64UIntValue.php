bb598c1f13c42792e0fbef6322d8450a1ecccd9b
Joni Eskelinen <joni@mobie.fi>
2016-04-13 12:50:04 +0300

    Add fromNumber to Base64UIntValue trait

--- lib/JWX/JWK/Parameter/Feature/Base64UIntValue.php
+++ lib/JWX/JWK/Parameter/Feature/Base64UIntValue.php
@@ -9,6 +9,17 @@ use JWX\Util\Base64;
 trait Base64UIntValue
 {
 	/**
+	 * Initialize parameter from base10 number
+	 *
+	 * @param int|string $number
+	 * @return self
+	 */
+	public static function fromNumber($number) {
+		$data = BigInt::fromBase10($number)->base256();
+		return new static(Base64::urlEncode($data));
+	}
+	
+	/**
 	 * Get value as a number
 	 *
 	 * @return BigInt
