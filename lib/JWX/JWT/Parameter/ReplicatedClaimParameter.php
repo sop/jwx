8f47461b36aa34d25e7edfda6640aa2432969c73
Joni Eskelinen <joni@mobie.fi>
2016-04-14 09:42:13 +0300

    Add support for replicated claims in header

---
+++ lib/JWX/JWT/Parameter/ReplicatedClaimParameter.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace JWX\JWT\Parameter;
+
+use JWX\JWT\Claim\Claim;
+
+
+/**
+ * Parameter allowing claims to be inserted into header.
+ *
+ * @link https://tools.ietf.org/html/rfc7519#section-5.3
+ */
+class ReplicatedClaimParameter extends JWTParameter
+{
+	/**
+	 * Constructor
+	 *
+	 * @param Claim $claim
+	 */
+	public function __construct(Claim $claim) {
+		parent::__construct($claim->name(), $claim->value());
+	}
+}
