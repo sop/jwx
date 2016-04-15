610e99fa4db2dc929fb7eb093548634bedaa403d
Joni Eskelinen <joni@mobie.fi>
2016-04-15 11:20:22 +0300

    Move example JWK to file

--- test/tests/example/JWSUsingRS256Test.php
+++ test/tests/example/JWSUsingRS256Test.php
@@ -22,37 +22,7 @@ class JWSUsingRS256Test extends PHPUnit_Framework_TestCase
 		' "exp":1300819380,' . "\r\n" .
 		' "http://example.com/is_root":true}'; /* @formatter:on */
 	
-	private static $_jwk = /* @formatter:off */
-	'{"kty":"RSA",
- "n":"ofgWCuLjybRlzo0tZWJjNiuSfb4p4fAkd_wWJcyQoTbji9k0l8W26mPddx'.
-      'HmfHQp-Vaw-4qPCJrcS2mJPMEzP1Pt0Bm4d4QlL-yRT-SFd2lZS-pCgNMs'.
-      'D1W_YpRPEwOWvG6b32690r2jZ47soMZo9wGzjb_7OMg0LOL-bSf63kpaSH'.
-      'SXndS5z5rexMdbBYUsLA9e-KXBdQOS-UTo7WTBEMa2R2CapHg665xsmtdV'.
-      'MTBQY4uDZlxvb3qCo5ZwKh9kG4LT6_I5IhlJH7aGhyxXFvUK-DWNmoudF8'.
-      'NAco9_h9iaGNj8q2ethFkMLs91kzk2PAcDTW9gb54h4FRWyuXpoQ",
- "e":"AQAB",
- "d":"Eq5xpGnNCivDflJsRQBXHx1hdR1k6Ulwe2JZD50LpXyWPEAeP88vLNO97I'.
-      'jlA7_GQ5sLKMgvfTeXZx9SE-7YwVol2NXOoAJe46sui395IW_GO-pWJ1O0'.
-      'BkTGoVEn2bKVRUCgu-GjBVaYLU6f3l9kJfFNS3E0QbVdxzubSu3Mkqzjkn'.
-      '439X0M_V51gfpRLI9JYanrC4D4qAdGcopV_0ZHHzQlBjudU2QvXt4ehNYT'.
-      'CBr6XCLQUShb1juUO1ZdiYoFaFQT5Tw8bGUl_x_jTj3ccPDVZFD9pIuhLh'.
-      'BOneufuBiB4cS98l2SR_RQyGWSeWjnczT0QU91p1DhOVRuOopznQ",
- "p":"4BzEEOtIpmVdVEZNCqS7baC4crd0pqnRH_5IB3jw3bcxGn6QLvnEtfdUdi'.
-      'YrqBdss1l58BQ3KhooKeQTa9AB0Hw_Py5PJdTJNPY8cQn7ouZ2KKDcmnPG'.
-      'BY5t7yLc1QlQ5xHdwW1VhvKn-nXqhJTBgIPgtldC-KDV5z-y2XDwGUc",
- "q":"uQPEfgmVtjL0Uyyx88GZFF1fOunH3-7cepKmtH4pxhtCoHqpWmT8YAmZxa'.
-      'ewHgHAjLYsp1ZSe7zFYHj7C6ul7TjeLQeZD_YwD66t62wDmpe_HlB-TnBA'.
-      '-njbglfIsRLtXlnDzQkv5dTltRJ11BKBBypeeF6689rjcJIDEz9RWdc",
- "dp":"BwKfV3Akq5_MFZDFZCnW-wzl-CCo83WoZvnLQwCTeDv8uzluRSnm71I3Q'.
-      'CLdhrqE2e9YkxvuxdBfpT_PI7Yz-FOKnu1R6HsJeDCjn12Sk3vmAktV2zb'.
-      '34MCdy7cpdTh_YVr7tss2u6vneTwrA86rZtu5Mbr1C1XsmvkxHQAdYo0",
- "dq":"h_96-mK1R_7glhsum81dZxjTnYynPbZpHziZjeeHcXYsXaaMwkOlODsWa'.
-      '7I9xXDoRwbKgB719rrmI2oKr6N3Do9U0ajaHF-NKJnwgjMd2w9cjz3_-ky'.
-      'NlxAr2v4IKhGNpmM5iIgOS1VZnOZ68m6_pbLBSp3nssTdlqvd0tIiTHU",
- "qi":"IYd7DHOhrWvxkwPQsRM2tOgrjbcrfvtQJipd-DlcxyVuuM9sQLdgjVk2o'.
-      'y26F0EmpScGLq2MowX7fhd_QJQ3ydy5cY7YIBi87w93IKLEdfnbJtoOPLU'.
-      'W0ITrJReOgo1cq9SbsxYawBgfp_gh6A5603k2-ZQwVK0JKSHuLFkuQ3U"
-}'; /* @formatter:on */
+	private static $_jwk;
 	
 	private static $_payloadBytes = [101, 121, 74, 104, 98, 71, 99, 105, 79, 
 		105, 74, 83, 85, 122, 73, 49, 78, 105, 74, 57, 46, 101, 121, 74, 112, 99, 
@@ -80,6 +50,11 @@ class JWSUsingRS256Test extends PHPUnit_Framework_TestCase
 		200, 242, 122, 122, 45, 184, 6, 99, 209, 108, 247, 202, 234, 86, 222, 64, 
 		92, 178, 33, 90, 69, 178, 194, 85, 102, 181, 90, 193, 167, 72, 160, 112, 
 		223, 200, 163, 42, 70, 149, 67, 208, 25, 238, 251, 71];
+	
+	public static function setUpBeforeClass() {
+		self::$_jwk = RSAPrivateKeyJWK::fromJSON(
+			file_get_contents(TEST_ASSETS_DIR . "/example/rfc7515-a2-jwk.json"));
+	}
 	
 	public function testEncodeHeader() {
 		static $expected = "eyJhbGciOiJSUzI1NiJ9";
@@ -110,8 +85,7 @@ class JWSUsingRS256Test extends PHPUnit_Framework_TestCase
 	 * @param string $payload
 	 */
 	public function testSign($payload) {
-		$jwk = RSAPrivateKeyJWK::fromJSON(self::$_jwk);
-		$algo = RS256Algorithm::fromPrivateKey($jwk);
+		$algo = RS256Algorithm::fromPrivateKey(self::$_jwk);
 		$signature = $algo->computeSignature($payload);
 		$expected = implode("", array_map("chr", self::$_signatureBytes));
 		$this->assertEquals($expected, $signature);
@@ -126,8 +100,7 @@ class JWSUsingRS256Test extends PHPUnit_Framework_TestCase
 	 * @param string $signature
 	 */
 	public function testValidate($payload, $signature) {
-		$jwk = RSAPrivateKeyJWK::fromJSON(self::$_jwk);
-		$algo = RS256Algorithm::fromPublicKey($jwk->publicKey());
+		$algo = RS256Algorithm::fromPublicKey(self::$_jwk->publicKey());
 		$this->assertTrue($algo->validateSignature($payload, $signature));
 	}
 }
