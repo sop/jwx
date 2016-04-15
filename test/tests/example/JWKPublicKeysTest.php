d009cb6e1704702ae8ec4d0553dfa8b756df7eed
Joni Eskelinen <joni@mobie.fi>
2016-04-15 11:25:58 +0300

    Move JWK to file

--- test/tests/example/JWKPublicKeysTest.php
+++ test/tests/example/JWKPublicKeysTest.php
@@ -23,36 +23,13 @@ use JWX\JWK\Parameter\ExponentParameter;
  */
 class JWKPublicKeysTest extends PHPUnit_Framework_TestCase
 {
-	private static $_data = // @formatter:off
-'{"keys":
-  [
-    {"kty":"EC",
-     "crv":"P-256",
-     "x":"MKBCTNIcKUSDii11ySs3526iDZ8AiTo7Tu6KPAqv7D4",
-     "y":"4Etl6SRW2YiLUrN5vfvVHuhp7x8PxltmWWlbbM4IFyM",
-     "use":"enc",
-     "kid":"1"},
-
-    {"kty":"RSA",
-     "n": "0vx7agoebGcQSuuPiLJXZptN9nndrQmbXEps2aiAFbWhM78LhWx'.
-'4cbbfAAtVT86zwu1RK7aPFFxuhDR1L6tSoc_BJECPebWKRXjBZCiFV4n3oknjhMs'.
-'tn64tZ_2W-5JsGY4Hc5n9yBXArwl93lqt7_RN5w6Cf0h4QyQ5v-65YGjQR0_FDW2'.
-'QvzqY368QQMicAtaSqzs8KJZgnYb9c7d0zgdAZHzu6qMQvRL5hajrn1n91CbOpbI'.
-'SD08qNLyrdkt-bFTWhAI4vMQFh6WeZu0fM4lFd2NcRwr3XPksINHaQ-G_xBniIqb'.
-'w0Ls1jF44-csFCur-kEgU8awapJzKnqDKgw",
-     "e":"AQAB",
-     "alg":"RS256",
-     "kid":"2011-04-29"}
-  ]
-}'; // @formatter:on
-	
-
 	/**
 	 *
 	 * @return JWKSet
 	 */
 	public function testJWKSet() {
-		$jwkset = JWKSet::fromJSON(self::$_data);
+		$jwkset = JWKSet::fromJSON(
+			file_get_contents(TEST_ASSETS_DIR . "/example/rfc7517-a1-jwk.json"));
 		$this->assertInstanceOf(JWKSet::class, $jwkset);
 		return $jwkset;
 	}
