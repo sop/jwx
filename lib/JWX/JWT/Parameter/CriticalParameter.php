e969c9818394ebe1d17a98827af8c20142fb8dd2
Joni Eskelinen <joni@mobie.fi>
2016-04-18 10:23:09 +0300

    Add methods to critical parameter

--- lib/JWX/JWT/Parameter/CriticalParameter.php
+++ lib/JWX/JWT/Parameter/CriticalParameter.php
@@ -25,4 +25,27 @@ class CriticalParameter extends RegisteredJWTParameter
 		}
 		return new static(...$value);
 	}
+	
+	/**
+	 * Get self with parameter name added
+	 *
+	 * @param string $name
+	 * @return self
+	 */
+	public function withParamName($name) {
+		$obj = clone $this;
+		$obj->_value[] = $name;
+		$obj->_value = array_values(array_unique($obj->_value));
+		return $obj;
+	}
+	
+	/**
+	 * Check whether given parameter name is critical
+	 *
+	 * @param string $name
+	 * @return bool
+	 */
+	public function has($name) {
+		return false !== array_search($name, $this->_value);
+	}
 }
