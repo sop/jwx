b616ac9762431e4304e0e0efc0e181ecc43f2ab9
Joni Eskelinen <joni@mobie.fi>
2016-04-19 15:49:25 +0300

    Add tests

---
+++ test/cookbook/jws/4.1.php
@@ -0,0 +1,64 @@
+<?php
+
+use JWX\JWT\Header;
+use JWX\JWS\JWS;
+use JWX\JWS\Algorithm\RS256Algorithm;
+use JWX\JWK\JWK;
+use JWX\JWK\RSA\RSAPrivateKeyJWK;
+use JWX\Util\Base64;
+
+
+class CookbookRSAv15SignatureTest extends PHPUnit_Framework_TestCase
+{
+	private static $_testData;
+	
+	public static function setUpBeforeClass() {
+		$json = file_get_contents(
+			COOKBOOK_DIR . "/jws/4_1.rsa_v15_signature.json");
+		self::$_testData = json_decode($json, true);
+	}
+	
+	public static function tearDownAfterClass() {
+		self::$_testData = null;
+	}
+	
+	public function testPrivateKey() {
+		$jwk = RSAPrivateKeyJWK::fromArray(self::$_testData["input"]["key"]);
+		$this->assertInstanceOf(RSAPrivateKeyJWK::class, $jwk);
+		return $jwk;
+	}
+	
+	public function testHeader() {
+		$header = Header::fromArray(self::$_testData["signing"]["protected"]);
+		$encoded = Base64::urlEncode($header->toJSON());
+		$this->assertEquals(self::$_testData["signing"]["protected_b64u"], 
+			$encoded);
+		return $header;
+	}
+	
+	/**
+	 * @depends testPrivateKey
+	 * @depends testHeader
+	 *
+	 * @param RSAPrivateKeyJWK $jwk
+	 * @param Header $header
+	 */
+	public function testSign(RSAPrivateKeyJWK $jwk, Header $header) {
+		$payload = self::$_testData["input"]["payload"];
+		$algo = RS256Algorithm::fromPrivateKey($jwk);
+		$jws = JWS::sign($payload, $algo, $header);
+		$this->assertEquals(self::$_testData["signing"]["sig"], 
+			Base64::urlEncode($jws->signature()));
+		return $jws;
+	}
+	
+	/**
+	 * @depends testSign
+	 *
+	 * @param JWS $jws
+	 */
+	public function testCompact(JWS $jws) {
+		$this->assertEquals(self::$_testData["output"]["compact"], 
+			$jws->toCompact());
+	}
+}
---
+++ test/cookbook/jws/4.4.php
@@ -0,0 +1,64 @@
+<?php
+
+use JWX\JWT\Header;
+use JWX\JWS\JWS;
+use JWX\JWS\Algorithm\HS256Algorithm;
+use JWX\JWK\JWK;
+use JWX\Util\Base64;
+use JWX\JWK\SymmetricKeyJWK;
+
+
+class CookbookHMACSHA2IntegrityProtectionTest extends PHPUnit_Framework_TestCase
+{
+	private static $_testData;
+	
+	public static function setUpBeforeClass() {
+		$json = file_get_contents(
+			COOKBOOK_DIR . "/jws/4_4.hmac-sha2_integrity_protection.json");
+		self::$_testData = json_decode($json, true);
+	}
+	
+	public static function tearDownAfterClass() {
+		self::$_testData = null;
+	}
+	
+	public function testSymmetricKey() {
+		$jwk = SymmetricKeyJWK::fromArray(self::$_testData["input"]["key"]);
+		$this->assertInstanceOf(SymmetricKeyJWK::class, $jwk);
+		return $jwk;
+	}
+	
+	public function testHeader() {
+		$header = Header::fromArray(self::$_testData["signing"]["protected"]);
+		$encoded = Base64::urlEncode($header->toJSON());
+		$this->assertEquals(self::$_testData["signing"]["protected_b64u"], 
+			$encoded);
+		return $header;
+	}
+	
+	/**
+	 * @depends testSymmetricKey
+	 * @depends testHeader
+	 *
+	 * @param SymmetricKeyJWK $jwk
+	 * @param Header $header
+	 */
+	public function testSign(SymmetricKeyJWK $jwk, Header $header) {
+		$payload = self::$_testData["input"]["payload"];
+		$algo = new HS256Algorithm($jwk->key());
+		$jws = JWS::sign($payload, $algo, $header);
+		$this->assertEquals(self::$_testData["signing"]["sig"], 
+			Base64::urlEncode($jws->signature()));
+		return $jws;
+	}
+	
+	/**
+	 * @depends testSign
+	 *
+	 * @param JWS $jws
+	 */
+	public function testCompact(JWS $jws) {
+		$this->assertEquals(self::$_testData["output"]["compact"], 
+			$jws->toCompact());
+	}
+}
