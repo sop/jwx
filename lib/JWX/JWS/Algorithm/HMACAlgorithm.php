2fe6a059e524094d70c80e16ce5c9a1365e4723d
Joni Eskelinen <joni@mobie.fi>
2016-04-20 08:33:27 +0300

    Add fromJWK method to HMACAlgorithm

--- lib/JWX/JWS/Algorithm/HMACAlgorithm.php
+++ lib/JWX/JWS/Algorithm/HMACAlgorithm.php
@@ -3,6 +3,10 @@
 namespace JWX\JWS\Algorithm;
 
 use JWX\JWS\SignatureAlgorithm;
+use JWX\JWA\JWA;
+use JWX\JWK\JWK;
+use JWX\JWK\Parameter\RegisteredJWKParameter;
+use JWX\JWK\Parameter\KeyTypeParameter;
 
 
 /**
@@ -33,6 +37,39 @@ abstract class HMACAlgorithm implements SignatureAlgorithm
 	 */
 	public function __construct($key) {
 		$this->_key = $key;
+	}
+	
+	/**
+	 * Initialize from JWK
+	 *
+	 * @param JWK $jwk
+	 * @throws \UnexpectedValueException
+	 * @return self
+	 */
+	public static function fromJWK(JWK $jwk) {
+		static $params = array(RegisteredJWKParameter::PARAM_KEY_TYPE, 
+			RegisteredJWKParameter::PARAM_ALGORITHM, 
+			RegisteredJWKParameter::PARAM_KEY_VALUE);
+		// check that all the parameters are present
+		if (!$jwk->has(...$params)) {
+			throw new \UnexpectedValueException("Missing parameters");
+		}
+		// check that key type is correct
+		$kty = $jwk->get(RegisteredJWKParameter::PARAM_KEY_TYPE)->value();
+		if ($kty != KeyTypeParameter::TYPE_OCT) {
+			throw new \UnexpectedValueException("Invalid key type");
+		}
+		$alg = $jwk->get(RegisteredJWKParameter::PARAM_ALGORITHM)->value();
+		$key = $jwk->get(RegisteredJWKParameter::PARAM_KEY_VALUE)->key();
+		switch ($alg) {
+		case JWA::ALGO_HS256:
+			return new HS256Algorithm($key);
+		case JWA::ALGO_HS384:
+			return new HS384Algorithm($key);
+		case JWA::ALGO_HS512:
+			return new HS512Algorithm($key);
+		}
+		throw new \UnexpectedValueException("Unsupported algorithm '$alg'");
 	}
 	
 	public function computeSignature($data) {
