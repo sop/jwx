cde0cfbc111b24477ff5416bdbaef3e5095789d3
Joni Eskelinen <joni@mobie.fi>
2016-04-20 10:42:46 +0300

    Add tests

---
+++ test/cookbook/jwe/5.1.php
@@ -0,0 +1,91 @@
+<?php
+
+use JWX\JWT\Header;
+use JWX\JWE\JWE;
+use JWX\JWE\KeyAlgorithm\RSAESPKCS1Algorithm;
+use JWX\JWE\EncryptionAlgorithm\A128CBCHS256Algorithm;
+use JWX\JWK\RSA\RSAPrivateKeyJWK;
+use JWX\Util\Base64;
+
+
+class CookbookKeyEncRSA15AndAESHMACSHA2Test extends PHPUnit_Framework_TestCase
+{
+	private static $_testData;
+	
+	public static function setUpBeforeClass() {
+		$json = file_get_contents(
+			COOKBOOK_DIR .
+				 "/jwe/5_1.key_encryption_using_rsa_v15_and_aes-hmac-sha2.json");
+		self::$_testData = json_decode($json, true);
+	}
+	
+	public static function tearDownAfterClass() {
+		self::$_testData = null;
+	}
+	
+	public function testPrivateKey() {
+		$jwk = RSAPrivateKeyJWK::fromArray(self::$_testData["input"]["key"]);
+		$this->assertInstanceOf(RSAPrivateKeyJWK::class, $jwk);
+		return $jwk;
+	}
+	
+	/**
+	 * @depends testPrivateKey
+	 *
+	 * Encryption result cannot be verified since RSAES uses random salt.
+	 *
+	 * @param RSAPrivateKeyJWK $jwk
+	 */
+	public function testEncryptedKey(RSAPrivateKeyJWK $jwk) {
+		$cek = Base64::urlDecode(self::$_testData["generated"]["cek"]);
+		$algo = RSAESPKCS1Algorithm::fromPrivateKey($jwk);
+		$ciphertext = $algo->encrypt($cek);
+		// test that decrypt succeeds
+		$result = $algo->decrypt($ciphertext);
+		$this->assertEquals($cek, $result);
+	}
+	
+	public function testHeader() {
+		$header = Header::fromArray(
+			self::$_testData["encrypting_content"]["protected"]);
+		$encoded = Base64::urlEncode($header->toJSON());
+		$this->assertEquals(
+			self::$_testData["encrypting_content"]["protected_b64u"], $encoded);
+		return $header;
+	}
+	
+	/**
+	 * @depends testHeader
+	 *
+	 * @param Header $header
+	 */
+	public function testContentEncryption(Header $header) {
+		$plaintext = self::$_testData["input"]["plaintext"];
+		$cek = Base64::urlDecode(self::$_testData["generated"]["cek"]);
+		$iv = Base64::urlDecode(self::$_testData["generated"]["iv"]);
+		$aad = Base64::urlEncode($header->toJSON());
+		$algo = new A128CBCHS256Algorithm();
+		list($ciphertext, $auth_tag) = $algo->encrypt($plaintext, $cek, $iv, 
+			$aad);
+		$this->assertEquals(
+			self::$_testData["encrypting_content"]["ciphertext"], 
+			Base64::urlEncode($ciphertext));
+		$this->assertEquals(self::$_testData["encrypting_content"]["tag"], 
+			Base64::urlEncode($auth_tag));
+	}
+	
+	/**
+	 * @depends testPrivateKey
+	 * @depends testHeader
+	 */
+	public function testCreateJWE(RSAPrivateKeyJWK $jwk, Header $header) {
+		$payload = self::$_testData["input"]["plaintext"];
+		$cek = Base64::urlDecode(self::$_testData["generated"]["cek"]);
+		$iv = Base64::urlDecode(self::$_testData["generated"]["iv"]);
+		$key_algo = RSAESPKCS1Algorithm::fromPrivateKey($jwk);
+		$enc_algo = new A128CBCHS256Algorithm();
+		$jwe = JWE::encrypt($payload, $cek, $key_algo, $enc_algo, $header, $iv);
+		$this->assertInstanceOf(JWE::class, $jwe);
+		return $jwe;
+	}
+}
---
+++ test/cookbook/jwe/5.3.php
@@ -0,0 +1,92 @@
+<?php
+
+use JWX\JWT\Header;
+use JWX\JWE\JWE;
+use JWX\JWE\EncryptionAlgorithm\A128CBCHS256Algorithm;
+use JWX\Util\Base64;
+use JWX\JWE\KeyAlgorithm\PBES2Algorithm;
+
+
+class CookbookKeyWrapPBES2AndAESHMACSHA2Test extends PHPUnit_Framework_TestCase
+{
+	private static $_testData;
+	
+	public static function setUpBeforeClass() {
+		$json = file_get_contents(
+			COOKBOOK_DIR .
+				 "/jwe/5_3.key_wrap_using_pbes2-aes-keywrap_with-aes-cbc-hmac-sha2.json");
+		self::$_testData = json_decode($json, true);
+	}
+	
+	public static function tearDownAfterClass() {
+		self::$_testData = null;
+	}
+	
+	public function testHeader() {
+		$header = Header::fromArray(
+			self::$_testData["encrypting_content"]["protected"]);
+		$encoded = Base64::urlEncode($header->toJSON());
+		$this->assertEquals(
+			self::$_testData["encrypting_content"]["protected_b64u"], $encoded);
+		return $header;
+	}
+	
+	/**
+	 * @depends testHeader
+	 *
+	 * @param Header $header
+	 */
+	public function testEncryptedKey(Header $header) {
+		$cek = Base64::urlDecode(self::$_testData["generated"]["cek"]);
+		$password = self::$_testData["input"]["pwd"];
+		$algo = PBES2Algorithm::fromHeader($header, $password);
+		$ciphertext = $algo->encrypt($cek);
+		$this->assertEquals(self::$_testData["encrypting_key"]["encrypted_key"], 
+			Base64::urlEncode($ciphertext));
+	}
+	
+	/**
+	 * @depends testHeader
+	 *
+	 * @param Header $header
+	 */
+	public function testContentEncryption(Header $header) {
+		$plaintext = self::$_testData["input"]["plaintext"];
+		$cek = Base64::urlDecode(self::$_testData["generated"]["cek"]);
+		$iv = Base64::urlDecode(self::$_testData["generated"]["iv"]);
+		$aad = Base64::urlEncode($header->toJSON());
+		$algo = new A128CBCHS256Algorithm();
+		list($ciphertext, $auth_tag) = $algo->encrypt($plaintext, $cek, $iv, 
+			$aad);
+		$this->assertEquals(
+			self::$_testData["encrypting_content"]["ciphertext"], 
+			Base64::urlEncode($ciphertext));
+		$this->assertEquals(self::$_testData["encrypting_content"]["tag"], 
+			Base64::urlEncode($auth_tag));
+	}
+	
+	/**
+	 * @depends testHeader
+	 */
+	public function testCreateJWE(Header $header) {
+		$payload = self::$_testData["input"]["plaintext"];
+		$cek = Base64::urlDecode(self::$_testData["generated"]["cek"]);
+		$iv = Base64::urlDecode(self::$_testData["generated"]["iv"]);
+		$password = self::$_testData["input"]["pwd"];
+		$key_algo = PBES2Algorithm::fromHeader($header, $password);
+		$enc_algo = new A128CBCHS256Algorithm();
+		$jwe = JWE::encrypt($payload, $cek, $key_algo, $enc_algo, $header, $iv);
+		$this->assertInstanceOf(JWE::class, $jwe);
+		return $jwe;
+	}
+	
+	/**
+	 * @depends testCreateJWE
+	 *
+	 * @param JWE $jwe
+	 */
+	public function testCompact(JWE $jwe) {
+		$this->assertEquals(self::$_testData["output"]["compact"], 
+			$jwe->toCompact());
+	}
+}
