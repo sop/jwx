e3ccb50b3379b9c8eef935de4b7eb5194d012f6a
Joni Eskelinen <joni@mobie.fi>
2016-05-04 14:52:06 +0300

    Add interface for compression algorithms

---
+++ lib/JWX/JWT/Parameter/CompressionAlgorithmParameterValue.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace JWX\JWT\Parameter;
+
+
+/**
+ * Interface for algorithms providing value for 'zip' header parameter.
+ */
+interface CompressionAlgorithmParameterValue
+{
+	/**
+	 * Get compression algorithm type as an 'zip' parameter value.
+	 *
+	 * @return string
+	 */
+	public function compressionParamValue();
+}
