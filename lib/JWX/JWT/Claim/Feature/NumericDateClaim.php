481898f1647acb6a839319dd842fafcc8717d1e4
Joni Eskelinen <joni@mobie.fi>
2016-05-12 15:40:20 +0300

    Add parameter validation

--- lib/JWX/JWT/Claim/Feature/NumericDateClaim.php
+++ lib/JWX/JWT/Claim/Feature/NumericDateClaim.php
@@ -13,11 +13,18 @@ trait NumericDateClaim
 	 *
 	 * @param string $time <code>strtotime</code> compatible time string
 	 * @param string $tz Default timezone
+	 * @throws \RuntimeException
 	 * @return static
 	 */
 	public static function fromString($time, $tz = "UTC") {
-		$dt = new \DateTimeImmutable($time, new \DateTimeZone($tz));
-		return new static($dt->getTimestamp());
+		try {
+			$dt = new \DateTimeImmutable($time, self::_createTimeZone($tz));
+			return new static($dt->getTimestamp());
+		} catch (\Exception $e) {
+			throw new \RuntimeException(
+				"Failed to create DateTime: " .
+					 self::_getLastDateTimeImmutableErrorsStr(), 0, $e);
+		}
 	}
 	
 	/**
@@ -32,10 +39,43 @@ trait NumericDateClaim
 	/**
 	 * Get date as a datetime object.
 	 *
+	 * @param string $tz Timezone
+	 * @throws \RuntimeException
 	 * @return \DateTimeImmutable
 	 */
-	public function dateTime() {
-		return \DateTimeImmutable::createFromFormat("!U", $this->_value, 
-			new \DateTimeZone("UTC"));
+	public function dateTime($tz = "UTC") {
+		$dt = \DateTimeImmutable::createFromFormat("!U", $this->_value, 
+			self::_createTimeZone($tz));
+		if (false === $dt) {
+			throw new \RuntimeException(
+				"Failed to create DateTime: " .
+					 self::_getLastDateTimeImmutableErrorsStr());
+		}
+		return $dt;
+	}
+	
+	/**
+	 * Create DateTimeZone object from string.
+	 *
+	 * @param string $tz
+	 * @throws \UnexpectedValueException
+	 * @return \DateTimeZone
+	 */
+	private static function _createTimeZone($tz) {
+		try {
+			return new \DateTimeZone($tz);
+		} catch (\Exception $e) {
+			throw new \UnexpectedValueException("Invalid timezone.", 0, $e);
+		}
+	}
+	
+	/**
+	 * Get last error caused by DateTimeImmutable.
+	 *
+	 * @return string
+	 */
+	private static function _getLastDateTimeImmutableErrorsStr() {
+		$errors = \DateTimeImmutable::getLastErrors()["errors"];
+		return implode(", ", $errors);
 	}
 }
