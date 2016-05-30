9fa36de91d81b99930d9e879d3523674e53512af
Joni Eskelinen <joni@mobie.fi>
2016-05-30 13:41:26 +0300

    Add tests

--- test/unit/jwe/key/AESGCMKWTest.php
+++ test/unit/jwe/key/AESGCMKWTest.php
@@ -1,6 +1,12 @@
 <?php
 
+use JWX\JWA\JWA;
 use JWX\JWE\KeyAlgorithm\A128GCMKWAlgorithm;
+use JWX\JWE\KeyAlgorithm\AESGCMKWAlgorithm;
+use JWX\JWK\JWK;
+use JWX\JWK\Parameter\AlgorithmParameter;
+use JWX\JWK\Parameter\KeyTypeParameter;
+use JWX\JWK\Parameter\KeyValueParameter;
 use JWX\JWT\Parameter\JWTParameter;
 
 
@@ -15,7 +21,7 @@ class AESGCMKWTest extends PHPUnit_Framework_TestCase
 	const IV = "123456789 12";
 	
 	public function testHeaderParams() {
-		$algo = new A128GCMKWAlgorithm(selF::KEY_128, self::IV);
+		$algo = new A128GCMKWAlgorithm(self::KEY_128, self::IV);
 		$params = $algo->headerParameters();
 		$this->assertContainsOnlyInstancesOf(JWTParameter::class, $params);
 	}
@@ -31,7 +37,26 @@ class AESGCMKWTest extends PHPUnit_Framework_TestCase
 	 * @expectedException RuntimeException
 	 */
 	public function testDecryptMissingAuthTag() {
-		$algo = new A128GCMKWAlgorithm(selF::KEY_128, self::IV);
+		$algo = new A128GCMKWAlgorithm(self::KEY_128, self::IV);
 		$algo->decrypt("");
+	}
+	
+	/**
+	 * @expectedException UnexpectedValueException
+	 */
+	public function testFromJWKNoAlgo() {
+		$jwk = new JWK(new KeyTypeParameter(KeyTypeParameter::TYPE_OCT), 
+			KeyValueParameter::fromString(self::KEY_128));
+		AESGCMKWAlgorithm::fromJWK($jwk, self::IV);
+	}
+	
+	/**
+	 * @expectedException UnexpectedValueException
+	 */
+	public function testFromJWKUnsupportedAlgo() {
+		$jwk = new JWK(new AlgorithmParameter(JWA::ALGO_NONE), 
+			new KeyTypeParameter(KeyTypeParameter::TYPE_OCT), 
+			KeyValueParameter::fromString(self::KEY_128));
+		AESGCMKWAlgorithm::fromJWK($jwk, self::IV);
 	}
 }
