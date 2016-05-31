506acef082c3b7f2f29926c1894d0074343d5a78
Joni Eskelinen <joni@mobie.fi>
2016-05-31 10:33:11 +0300

    Rename RSAOAEPAlgorithm to RSAESOAEPAlgorithm

--- lib/JWX/JWE/KeyAlgorithm/RSAESOAEPAlgorithm.php
+++ lib/JWX/JWE/KeyAlgorithm/RSAESOAEPAlgorithm.php
@@ -10,7 +10,7 @@ use JWX\JWA\JWA;
  *
  * @link https://tools.ietf.org/html/rfc7518#section-4.3
  */
-class RSAOAEPAlgorithm extends RSAESKeyAlgorithm
+class RSAESOAEPAlgorithm extends RSAESKeyAlgorithm
 {
 	protected function _paddingScheme() {
 		return OPENSSL_PKCS1_OAEP_PADDING;
