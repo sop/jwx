63ec2f1f3b70bdedea2ddabca098c53e48107d13
Joni Eskelinen <joni@mobie.fi>
2016-06-08 09:40:50 +0300

    Add type checking method

--- lib/JWX/JWT/Header/TypedHeader.php
+++ lib/JWX/JWT/Header/TypedHeader.php
@@ -4,9 +4,15 @@ namespace JWX\JWT\Header;
 
 use JWX\JWT\Parameter\AlgorithmParameter;
 use JWX\JWT\Parameter\AuthenticationTagParameter;
+use JWX\JWT\Parameter\B64PayloadParameter;
+use JWX\JWT\Parameter\JWTParameter;
 use JWX\JWT\Parameter\RegisteredJWTParameter;
 
 
+/**
+ * Trait for Header to provide parameter accessor methods for typed return
+ * types.
+ */
 trait TypedHeader
 {
 	/**
@@ -26,6 +32,7 @@ trait TypedHeader
 	abstract public function get($name);
 	
 	/**
+	 * Check whether the algorithm parameter is present.
 	 *
 	 * @return bool
 	 */
@@ -34,19 +41,18 @@ trait TypedHeader
 	}
 	
 	/**
+	 * Get the algorithm parameter.
 	 *
 	 * @throws \UnexpectedValueException
 	 * @return AlgorithmParameter
 	 */
 	public function algorithm() {
-		$param = $this->get(RegisteredJWTParameter::P_ALG);
-		if (!$param instanceof AlgorithmParameter) {
-			throw new \UnexpectedValueException();
-		}
-		return $param;
+		return $this->_checkType($this->get(RegisteredJWTParameter::P_ALG), 
+			AlgorithmParameter::class);
 	}
 	
 	/**
+	 * Check whether the authentication tag parameter is present.
 	 *
 	 * @return bool
 	 */
@@ -55,14 +61,47 @@ trait TypedHeader
 	}
 	
 	/**
+	 * Get the authentication tag parameter.
 	 *
 	 * @throws \UnexpectedValueException
 	 * @return AuthenticationTagParameter
 	 */
 	public function authenticationTag() {
-		$param = $this->get(RegisteredJWTParameter::P_TAG);
-		if (!$param instanceof AuthenticationTagParameter) {
-			throw new \UnexpectedValueException();
+		return $this->_checkType($this->get(RegisteredJWTParameter::P_TAG), 
+			AuthenticationTagParameter::class);
+	}
+	
+	/**
+	 * Check whether the 'base64url-encode payload' parameter is present.
+	 *
+	 * @return bool
+	 */
+	public function hasB64Payload() {
+		return $this->has(RegisteredJWTParameter::P_B64);
+	}
+	
+	/**
+	 * Get the 'base64url-encode payload' parameter.
+	 *
+	 * @throws \UnexpectedValueException
+	 * @return B64PayloadParameter
+	 */
+	public function b64Payload() {
+		return $this->_checkType($this->get(RegisteredJWTParameter::P_B64), 
+			B64PayloadParameter::class);
+	}
+	
+	/**
+	 *
+	 * @param JWTParameter $param
+	 * @param string $cls
+	 * @throws \UnexpectedValueException
+	 * @return JWTParameter
+	 */
+	private static function _checkType(JWTParameter $param, $cls) {
+		if (!$param instanceof $cls) {
+			throw new \UnexpectedValueException(
+				"$cls expected, got " . get_class($param));
 		}
 		return $param;
 	}
