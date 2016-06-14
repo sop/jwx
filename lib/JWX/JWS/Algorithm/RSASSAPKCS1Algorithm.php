b7e0df47317d23de8a8fa68e3f7ed5045a849a82
Joni Eskelinen <joni@mobie.fi>
2016-06-14 10:15:07 +0300

    Use OpenSSL signature base class

--- lib/JWX/JWS/Algorithm/RSASSAPKCS1Algorithm.php
+++ lib/JWX/JWS/Algorithm/RSASSAPKCS1Algorithm.php
@@ -6,7 +6,6 @@ use JWX\JWA\JWA;
 use JWX\JWK\JWK;
 use JWX\JWK\RSA\RSAPrivateKeyJWK;
 use JWX\JWK\RSA\RSAPublicKeyJWK;
-use JWX\JWS\SignatureAlgorithm;
 use JWX\JWT\Parameter\AlgorithmParameter;
 
 
@@ -15,22 +14,8 @@ use JWX\JWT\Parameter\AlgorithmParameter;
  *
  * @link https://tools.ietf.org/html/rfc7518#section-3.3
  */
-abstract class RSASSAPKCS1Algorithm implements SignatureAlgorithm
+abstract class RSASSAPKCS1Algorithm extends OpenSSLSignatureAlgorithm
 {
-	/**
-	 * Public key.
-	 *
-	 * @var RSAPublicKeyJWK $_publicKey
-	 */
-	protected $_publicKey;
-	
-	/**
-	 * Private key.
-	 *
-	 * @var RSAPrivateKeyJWK|null $_privateKey
-	 */
-	protected $_privateKey;
-	
 	/**
 	 * Mapping from algorithm name to class name.
 	 *
@@ -47,17 +32,7 @@ abstract class RSASSAPKCS1Algorithm implements SignatureAlgorithm
 	);
 	
 	/**
-	 * Get message digest method name supported by OpenSSL.
-	 *
-	 * @return string
-	 */
-	abstract protected function _mdMethod();
-	
-	/**
 	 * Constructor
-	 *
-	 * Use <b>fromPublicKey</b> or <b>fromPrivateKey</b>
-	 * static initializer instead!
 	 *
 	 * @param RSAPublicKeyJWK $pub_key
 	 * @param RSAPrivateKeyJWK $priv_key
@@ -121,56 +96,6 @@ abstract class RSASSAPKCS1Algorithm implements SignatureAlgorithm
 			return $cls::fromPublicKey(RSAPublicKeyJWK::fromJWK($jwk));
 		}
 		throw new \UnexpectedValueException("Not an RSA key.");
-	}
-	
-	public function computeSignature($data) {
-		/**
-		 * NOTE: OpenSSL uses PKCS #1 v1.5 padding by default, so no explicit
-		 * padding is required by sign and verify operations.
-		 */
-		if (!isset($this->_privateKey)) {
-			throw new \LogicException("Private key not set.");
-		}
-		$key = openssl_pkey_get_private($this->_privateKey->toPEM()->string());
-		if (!$key) {
-			throw new \RuntimeException(
-				"openssl_pkey_get_private() failed: " .
-					 $this->_getLastOpenSSLError());
-		}
-		$result = @openssl_sign($data, $signature, $key, $this->_mdMethod());
-		if (!$result) {
-			throw new \RuntimeException(
-				"openssl_sign() failed: " . $this->_getLastOpenSSLError());
-		}
-		return $signature;
-	}
-	
-	public function validateSignature($data, $signature) {
-		$key = openssl_pkey_get_public($this->_publicKey->toPEM()->string());
-		if (!$key) {
-			throw new \RuntimeException(
-				"openssl_pkey_get_public() failed: " .
-					 $this->_getLastOpenSSLError());
-		}
-		$result = @openssl_verify($data, $signature, $key, $this->_mdMethod());
-		if (false === $result || -1 == $result) {
-			throw new \RuntimeException(
-				"openssl_verify() failed: " . $this->_getLastOpenSSLError());
-		}
-		return $result == 1;
-	}
-	
-	/**
-	 * Get last OpenSSL error message.
-	 *
-	 * @return string|null
-	 */
-	protected function _getLastOpenSSLError() {
-		$msg = null;
-		while (false !== ($err = openssl_error_string())) {
-			$msg = $err;
-		}
-		return $msg;
 	}
 	
 	public function headerParameters() {
