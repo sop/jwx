7f352975d1576a933fc4de9deb6e4ae054e512ab
Joni Eskelinen <joni@mobie.fi>
2016-06-14 10:16:12 +0300

    Fully implement EC JWK's

--- lib/JWX/JWK/EC/ECPrivateKeyJWK.php
+++ lib/JWX/JWK/EC/ECPrivateKeyJWK.php
@@ -2,10 +2,19 @@
 
 namespace JWX\JWK\EC;
 
+use CryptoUtil\ASN1\EC\ECPrivateKey;
+use CryptoUtil\ASN1\EC\ECPublicKey;
+use CryptoUtil\Conversion\ECConversion;
+use CryptoUtil\PEM\PEM;
+use JWX\JWK\Feature\AsymmetricPrivateKey;
 use JWX\JWK\JWK;
+use JWX\JWK\Parameter\CurveParameter;
+use JWX\JWK\Parameter\ECCPrivateKeyParameter;
 use JWX\JWK\Parameter\JWKParameter;
 use JWX\JWK\Parameter\KeyTypeParameter;
 use JWX\JWK\Parameter\RegisteredJWKParameter;
+use JWX\JWK\Parameter\XCoordinateParameter;
+use JWX\JWK\Parameter\YCoordinateParameter;
 
 
 /**
@@ -15,7 +24,7 @@ use JWX\JWK\Parameter\RegisteredJWKParameter;
  * @link https://tools.ietf.org/html/rfc7518#section-6.2
  * @link https://tools.ietf.org/html/rfc7518#section-6.2.2
  */
-class ECPrivateKeyJWK extends JWK
+class ECPrivateKeyJWK extends JWK implements AsymmetricPrivateKey
 {
 	/**
 	 * Parameter names managed by this class.
@@ -49,5 +58,70 @@ class ECPrivateKeyJWK extends JWK
 		if ($this->keyTypeParameter()->value() != KeyTypeParameter::TYPE_EC) {
 			throw new \UnexpectedValueException("Invalid key type.");
 		}
+		// cast ECC private key parameter to correct class
+		$key = RegisteredJWKParameter::PARAM_ECC_PRIVATE_KEY;
+		$this->_parameters[$key] = new ECCPrivateKeyParameter(
+			$this->_parameters[$key]->value());
+	}
+	
+	/**
+	 * Initialize from ECPrivateKey.
+	 *
+	 * @param ECPrivateKey $pk
+	 * @throws \UnexpectedValueException
+	 * @return self
+	 */
+	public static function fromECPrivateKey(ECPrivateKey $pk) {
+		if (!$pk->hasNamedCurve()) {
+			throw new \UnexpectedValueException("No curve name.");
+		}
+		$curve = CurveParameter::fromOID($pk->namedCurve());
+		$pubkey = $pk->publicKey();
+		list($x, $y) = $pubkey->curvePointOctets();
+		$xcoord = XCoordinateParameter::fromString($x);
+		$ycoord = YCoordinateParameter::fromString($y);
+		$priv = ECCPrivateKeyParameter::fromString($pk->privateKeyOctets());
+		$key_type = new KeyTypeParameter(KeyTypeParameter::TYPE_EC);
+		return new self($key_type, $curve, $xcoord, $ycoord, $priv);
+	}
+	
+	/**
+	 * Initialize from PEM.
+	 *
+	 * @param PEM $pem
+	 * @return self
+	 */
+	public static function fromPEM(PEM $pem) {
+		return self::fromECPrivateKey(ECPrivateKey::fromPEM($pem));
+	}
+	
+	/**
+	 * Get the public key component of the EC private key.
+	 *
+	 * @return ECPublicKeyJWK
+	 */
+	public function publicKey() {
+		$kty = $this->keyTypeParameter();
+		$curve = $this->curveParameter();
+		$xcoord = $this->XCoordinateParameter();
+		$ycoord = $this->YCoordinateParameter();
+		return new ECPublicKeyJWK($kty, $curve, $xcoord, $ycoord);
+	}
+	
+	/**
+	 * Convert EC private key to PEM.
+	 *
+	 * @return PEM
+	 */
+	public function toPEM() {
+		$curve_oid = CurveParameter::nameToOID($this->curveParameter()->value());
+		$x = ECConversion::octetsToNumber(
+			$this->XCoordinateParameter()->coordinateOctets());
+		$y = ECConversion::octetsToNumber(
+			$this->YCoordinateParameter()->coordinateOctets());
+		$pubkey = ECPublicKey::fromCoordinates($x, $y, $curve_oid);
+		$priv = $this->ECCPrivateKeyParameter()->privateKeyOctets();
+		$ec = new ECPrivateKey($priv, $curve_oid, $pubkey->ECPoint());
+		return $ec->privateKeyInfo()->toPEM();
 	}
 }
--- lib/JWX/JWK/EC/ECPublicKeyJWK.php
+++ lib/JWX/JWK/EC/ECPublicKeyJWK.php
@@ -2,10 +2,17 @@
 
 namespace JWX\JWK\EC;
 
+use CryptoUtil\ASN1\EC\ECPublicKey;
+use CryptoUtil\Conversion\ECConversion;
+use CryptoUtil\PEM\PEM;
+use JWX\JWK\Feature\AsymmetricPublicKey;
 use JWX\JWK\JWK;
+use JWX\JWK\Parameter\CurveParameter;
 use JWX\JWK\Parameter\JWKParameter;
 use JWX\JWK\Parameter\KeyTypeParameter;
 use JWX\JWK\Parameter\RegisteredJWKParameter;
+use JWX\JWK\Parameter\XCoordinateParameter;
+use JWX\JWK\Parameter\YCoordinateParameter;
 
 
 /**
@@ -15,7 +22,7 @@ use JWX\JWK\Parameter\RegisteredJWKParameter;
  * @link https://tools.ietf.org/html/rfc7518#section-6.2
  * @link https://tools.ietf.org/html/rfc7518#section-6.2.1
  */
-class ECPublicKeyJWK extends JWK
+class ECPublicKeyJWK extends JWK implements AsymmetricPublicKey
 {
 	/**
 	 * Parameter names managed by this class.
@@ -46,5 +53,49 @@ class ECPublicKeyJWK extends JWK
 		if ($this->keyTypeParameter()->value() != KeyTypeParameter::TYPE_EC) {
 			throw new \UnexpectedValueException("Invalid key type.");
 		}
+	}
+	
+	/**
+	 * Initialize from ECPublicKey.
+	 *
+	 * @param ECPublicKey $pk
+	 * @throws \UnexpectedValueException
+	 * @return self
+	 */
+	public static function fromECPublicKey(ECPublicKey $pk) {
+		if (!$pk->hasNamedCurve()) {
+			throw new \UnexpectedValueException("No curve name.");
+		}
+		$curve = CurveParameter::fromOID($pk->namedCurve());
+		list($x, $y) = $pk->curvePointOctets();
+		$xcoord = XCoordinateParameter::fromString($x);
+		$ycoord = YCoordinateParameter::fromString($y);
+		$key_type = new KeyTypeParameter(KeyTypeParameter::TYPE_EC);
+		return new self($key_type, $curve, $xcoord, $ycoord);
+	}
+	
+	/**
+	 * Initialize from PEM.
+	 *
+	 * @param PEM $pem
+	 * @return self
+	 */
+	public static function fromPEM(PEM $pem) {
+		return self::fromECPublicKey(ECPublicKey::fromPEM($pem));
+	}
+	
+	/**
+	 * Convert EC public key to PEM.
+	 *
+	 * @return PEM
+	 */
+	public function toPEM() {
+		$curve_oid = CurveParameter::nameToOID($this->curveParameter()->value());
+		$x = ECConversion::octetsToNumber(
+			$this->XCoordinateParameter()->coordinateOctets());
+		$y = ECConversion::octetsToNumber(
+			$this->YCoordinateParameter()->coordinateOctets());
+		$ec = ECPublicKey::fromCoordinates($x, $y, $curve_oid);
+		return $ec->publicKeyInfo()->toPEM();
 	}
 }
