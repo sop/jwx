39682b738316d17fd30fe88c511da71f5468ea0d
Joni Eskelinen <joni@mobie.fi>
2016-06-16 15:52:32 +0300

    Fix examples conforming to new API

--- examples/jwe-consume.php
+++ examples/jwe-consume.php
@@ -7,8 +7,6 @@
  */
 
 use CryptoUtil\PEM\PEM;
-use JWX\JWE\EncryptionAlgorithm\A128CBCHS256Algorithm;
-use JWX\JWE\KeyAlgorithm\RSAESPKCS1Algorithm;
 use JWX\JWK\RSA\RSAPrivateKeyJWK;
 use JWX\JWT\JWT;
 use JWX\JWT\ValidationContext;
@@ -20,11 +18,10 @@ $jwt = new JWT($argv[1]);
 // load RSA private key
 $jwk = RSAPrivateKeyJWK::fromPEM(
 	PEM::fromFile(dirname(__DIR__) . "/test/assets/rsa/private_key.pem"));
-$key_algo = RSAESPKCS1Algorithm::fromPrivateKey($jwk);
-$enc_algo = new A128CBCHS256Algorithm();
-$ctx = new ValidationContext();
+// create validation context containing only key for decryption
+$ctx = ValidationContext::fromKey($jwk);
 // decrypt claims from the encrypted JWT
-$claims = $jwt->claimsFromJWE($key_algo, $enc_algo, $ctx);
+$claims = $jwt->claims($ctx);
 // print all claims
 foreach ($claims as $claim) {
 	echo $claim->name() . ": " . json_encode($claim->value()) . "\n";
--- examples/jws-consume.php
+++ examples/jws-consume.php
@@ -5,7 +5,7 @@
  * php jws-consume.php $(php jws-create.php)
  */
 
-use JWX\JWS\Algorithm\HS256Algorithm;
+use JWX\JWK\Symmetric\SymmetricKeyJWK;
 use JWX\JWT\JWT;
 use JWX\JWT\ValidationContext;
 
@@ -13,13 +13,15 @@ require dirname(__DIR__) . "/vendor/autoload.php";
 
 // read JWT token from the first argument
 $jwt = new JWT($argv[1]);
+// key to use for signature validation
+$jwk = SymmetricKeyJWK::fromKey("secret");
 // create validation context
-$ctx = (new ValidationContext())->withIssuer("John Doe")
+$ctx = ValidationContext::fromKey($jwk)->withIssuer("John Doe")
 	->withSubject("Jane Doe")
 	->withAudience("acme-client");
 // get claims set from the JWT. signature shall be verified and claims
 // validated according to validation context.
-$claims = $jwt->claimsFromJWS(new HS256Algorithm("secret"), $ctx);
+$claims = $jwt->claims($ctx);
 // print all claims
 foreach ($claims as $claim) {
 	echo $claim->name() . ": " . json_encode($claim->value()) . "\n";
