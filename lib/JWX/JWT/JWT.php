e33186708537e063eed0ac603d7d7fb28f58f797
Joni Eskelinen <joni@mobie.fi>
2016-06-16 12:21:43 +0300

    Simplify API

--- lib/JWX/JWT/JWT.php
+++ lib/JWX/JWT/JWT.php
@@ -6,6 +6,7 @@ use JWX\JWE\CompressionAlgorithm;
 use JWX\JWE\ContentEncryptionAlgorithm;
 use JWX\JWE\JWE;
 use JWX\JWE\KeyManagementAlgorithm;
+use JWX\JWK\JWKSet;
 use JWX\JWS\Algorithm\NoneAlgorithm;
 use JWX\JWS\JWS;
 use JWX\JWS\SignatureAlgorithm;
@@ -25,8 +26,22 @@ use JWX\Util\Base64;
  */
 class JWT
 {
-	// JWT type enumerations
+	/**
+	 * Type identifier for signed JWT.
+	 *
+	 * @internal
+	 *
+	 * @var int
+	 */
 	const TYPE_JWS = 0;
+	
+	/**
+	 * Type identifier for encrypted JWT.
+	 *
+	 * @internal
+	 *
+	 * @var int
+	 */
 	const TYPE_JWE = 1;
 	
 	/**
@@ -51,11 +66,14 @@ class JWT
 	 */
 	public function __construct($token) {
 		$this->_parts = explode(".", $token);
-		if (3 == count($this->_parts)) {
+		switch (count($this->_parts)) {
+		case 3:
 			$this->_type = self::TYPE_JWS;
-		} else if (5 == count($this->_parts)) {
+			break;
+		case 5:
 			$this->_type = self::TYPE_JWE;
-		} else {
+			break;
+		default:
 			throw new \UnexpectedValueException("Not a JWT token.");
 		}
 	}
@@ -106,6 +124,57 @@ class JWT
 			CompressionAlgorithm $zip_algo = null, Header $header = null) {
 		$payload = $claims->toJSON();
 		$jwe = JWE::encrypt($payload, $key_algo, $enc_algo, $zip_algo, $header);
+		return new self($jwe->toCompact());
+	}
+	
+	/**
+	 * Sign self producing a nested JWT.
+	 *
+	 * Note that if JWT is to be signed and encrypted, it should be done in
+	 * sign-then-encrypt order. Please refer to links for security information.
+	 *
+	 * @link https://tools.ietf.org/html/rfc7519#section-11.2
+	 * @param SignatureAlgorithm $algo Signature algorithm
+	 * @param Header|null $header Optional header
+	 * @throws \RuntimeException For generic errors
+	 * @return self
+	 */
+	public function signNested(SignatureAlgorithm $algo, Header $header = null) {
+		if (!isset($header)) {
+			$header = new Header();
+		}
+		// add JWT content type parameter
+		$header = $header->withParameters(
+			new ContentTypeParameter(ContentTypeParameter::TYPE_JWT));
+		$jws = JWS::sign($this->token(), $algo, $header);
+		return new self($jws->toCompact());
+	}
+	
+	/**
+	 * Encrypt self producing a nested JWT.
+	 *
+	 * This JWT should be a JWS, that is, the order of nesting should be
+	 * sign-then-encrypt.
+	 *
+	 * @link https://tools.ietf.org/html/rfc7519#section-11.2
+	 * @param KeyManagementAlgorithm $key_algo Key management algorithm
+	 * @param ContentEncryptionAlgorithm $enc_algo Content encryption algorithm
+	 * @param CompressionAlgorithm|null $zip_algo Optional compression algorithm
+	 * @param Header|null $header Optional header
+	 * @throws \RuntimeException For generic errors
+	 * @return self
+	 */
+	public function encryptNested(KeyManagementAlgorithm $key_algo, 
+			ContentEncryptionAlgorithm $enc_algo, 
+			CompressionAlgorithm $zip_algo = null, Header $header = null) {
+		if (!isset($header)) {
+			$header = new Header();
+		}
+		// add JWT content type parameter
+		$header = $header->withParameters(
+			new ContentTypeParameter(ContentTypeParameter::TYPE_JWT));
+		$jwe = JWE::encrypt($this->token(), $key_algo, $enc_algo, $zip_algo, 
+			$header);
 		return new self($jwe->toCompact());
 	}
 	
@@ -190,81 +259,78 @@ class JWT
 	}
 	
 	/**
-	 * Get claims from a signed JWS.
+	 * Get claims from the JWT.
 	 *
-	 * @param SignatureAlgorithm $algo Signature algorithm
-	 * @param ValidationContext $ctx Validation context
-	 * @throws ValidationException If validation fails
+	 * Claims shall be validated according to given validation context.
+	 * Validation context must contain all the necessary keys for the signature
+	 * validation and/or content decryption.
+	 *
+	 * @param ValidationContext $ctx
+	 * @throws ValidationException If signature is invalid, or decryption fails,
+	 *         or claims validation fails.
 	 * @throws \RuntimeException For generic errors
 	 * @return Claims
 	 */
-	public function claimsFromJWS(SignatureAlgorithm $algo, 
-			ValidationContext $ctx) {
-		$jws = $this->JWS();
-		if (!$jws->validate($algo)) {
+	public function claims(ValidationContext $ctx) {
+		$keys = $ctx->keys();
+		// check signature or decrypt depending on the JWT type.
+		if ($this->isJWS()) {
+			$payload = self::_validatedPayloadFromJWS($this->JWS(), $keys);
+		} else {
+			$payload = self::_validatedPayloadFromJWE($this->JWE(), $keys);
+		}
+		// if JWT contains a nested token
+		if ($this->isNested()) {
+			$jwt = new JWT($payload);
+			return $jwt->claims($ctx);
+		}
+		// decode claims and validate
+		$claims = Claims::fromJSON($payload);
+		$ctx->validate($claims);
+		return $claims;
+	}
+	
+	/**
+	 * Get payload from JWS.
+	 *
+	 * @param JWS $jws JWS
+	 * @param JWKSet $keys Set of keys usable for signature validation
+	 * @throws ValidationException If signature validation fails
+	 * @return string
+	 */
+	private static function _validatedPayloadFromJWS(JWS $jws, JWKSet $keys) {
+		try {
+			if (1 == count($keys)) {
+				$valid = $jws->validateWithJWK($keys->first());
+			} else {
+				$valid = $jws->validateWithJWKSet($keys);
+			}
+		} catch (\RuntimeException $e) {
+			throw new ValidationException("JWS validation failed.", null, $e);
+		}
+		if (!$valid) {
 			throw new ValidationException("JWS signature is invalid.");
 		}
-		$claims = Claims::fromJSON($jws->payload());
-		$ctx->validate($claims);
-		return $claims;
+		return $jws->payload();
 	}
 	
 	/**
-	 * Get claims from an encrypted JWE.
+	 * Get payload from JWE.
 	 *
-	 * @param KeyManagementAlgorithm $key_algo Key management algorithm
-	 * @param ContentEncryptionAlgorithm $enc_algo Content encryption algorithm
-	 * @param ValidationContext $ctx Validation context
-	 * @throws ValidationException If validation fails
-	 * @throws \RuntimeException For generic errors
-	 * @return Claims
+	 * @param JWE $jwe JWE
+	 * @param JWKSet $keys Set of keys usable for decryption
+	 * @throws ValidationException If decryption fails
+	 * @return string
 	 */
-	public function claimsFromJWE(KeyManagementAlgorithm $key_algo, 
-			ContentEncryptionAlgorithm $enc_algo, ValidationContext $ctx) {
-		$claims = Claims::fromJSON($this->JWE()->decrypt($key_algo, $enc_algo));
-		$ctx->validate($claims);
-		return $claims;
-	}
-	
-	/**
-	 * Encrypt JWT producing a nested JWT.
-	 *
-	 * @param KeyManagementAlgorithm $key_algo Key management algorithm
-	 * @param ContentEncryptionAlgorithm $enc_algo Content encryption algorithm
-	 * @param CompressionAlgorithm|null $zip_algo Optional compression algorithm
-	 * @param Header|null $header Optional header
-	 * @throws \RuntimeException For generic errors
-	 * @return self
-	 */
-	public function encryptNested(KeyManagementAlgorithm $key_algo, 
-			ContentEncryptionAlgorithm $enc_algo, 
-			CompressionAlgorithm $zip_algo = null, Header $header = null) {
-		if (!isset($header)) {
-			$header = new Header();
+	private static function _validatedPayloadFromJWE(JWE $jwe, JWKSet $keys) {
+		try {
+			if (1 == count($keys)) {
+				return $jwe->decryptWithJWK($keys->first());
+			}
+			return $jwe->decryptWithJWKSet($keys);
+		} catch (\RuntimeException $e) {
+			throw new ValidationException("JWE validation failed.", null, $e);
 		}
-		// add JWT content type parameter
-		$header = $header->withParameters(
-			new ContentTypeParameter(ContentTypeParameter::TYPE_JWT));
-		$payload = $this->token();
-		$jwe = JWE::encrypt($payload, $key_algo, $enc_algo, $zip_algo, $header);
-		return new self($jwe->toCompact());
-	}
-	
-	/**
-	 * Get nested JWT from an encrypted JWT.
-	 *
-	 * @param KeyManagementAlgorithm $key_algo Key management algorithm
-	 * @param ContentEncryptionAlgorithm $enc_algo Content encryption algorithm
-	 * @throws \RuntimeException For generic errors
-	 * @return self
-	 */
-	public function nestedFromJWE(KeyManagementAlgorithm $key_algo, 
-			ContentEncryptionAlgorithm $enc_algo) {
-		if (!$this->isNested()) {
-			throw new \UnexpectedValueException("Not a nested JWT token.");
-		}
-		$jwt = new JWT($this->JWE()->decrypt($key_algo, $enc_algo));
-		return $jwt;
 	}
 	
 	/**
