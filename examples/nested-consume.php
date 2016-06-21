efd4415dfc6511c7296de969e0f50c2abb26c0f0
Joni Eskelinen <joni@mobie.fi>
2016-06-21 09:39:29 +0300

    Add nested example

---
+++ examples/nested-consume.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Decrypt and verify signature of the JWT created by nested-create.php.
+ *
+ * php nested-consume.php $(php nested-create.php)
+ */
+
+use CryptoUtil\PEM\PEM;
+use JWX\JWK\EC\ECPublicKeyJWK;
+use JWX\JWK\JWKSet;
+use JWX\JWK\Symmetric\SymmetricKeyJWK;
+use JWX\JWT\Claim\RegisteredClaim;
+use JWX\JWT\JWT;
+use JWX\JWT\ValidationContext;
+
+require dirname(__DIR__) . "/vendor/autoload.php";
+
+// load EC public key
+$ec_pub_jwk = ECPublicKeyJWK::fromPEM(
+	PEM::fromFile(dirname(__DIR__) . "/test/assets/ec/public_key_P-256.pem"));
+// initialize symmetric key for key management algorithm
+$kek_jwk = SymmetricKeyJWK::fromKey("0123456789abcdef");
+// compose JWK set with identified keys
+$keys = new JWKSet($ec_pub_jwk->withKeyID("sig-key"), 
+	$kek_jwk->withKeyID("enc-key"));
+// read JWT token from the first argument
+$jwt = new JWT($argv[1]);
+// initialize validation context
+$ctx = new ValidationContext([RegisteredClaim::NAME_ISSUER => "joe"], $keys);
+// decrypt, verify signature and validate claims
+$claims = $jwt->claims($ctx);
+// print claims
+foreach ($claims as $claim) {
+	printf("%s: %s\n", $claim->name(), $claim->value());
+}
---
+++ examples/nested-create.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Produce a nested JWT token that is first signed and then encrypted.
+ * Signature shall be computed using an elliptic curve cryptography.
+ * Key management shall be done using AES-GCM key wrapping.
+ * Content shall be encrypted using AES in CBC mode with SHA-2 based integrity
+ * and authentication.
+ *
+ * php nested-create.php
+ */
+
+use CryptoUtil\PEM\PEM;
+use JWX\JWE\EncryptionAlgorithm\A128CBCHS256Algorithm;
+use JWX\JWE\KeyAlgorithm\A128GCMKWAlgorithm;
+use JWX\JWK\EC\ECPrivateKeyJWK;
+use JWX\JWS\Algorithm\ES256Algorithm;
+use JWX\JWT\Claim\Claim;
+use JWX\JWT\Claim\ExpirationTimeClaim;
+use JWX\JWT\Claim\IssuerClaim;
+use JWX\JWT\Claims;
+use JWX\JWT\JWT;
+
+require dirname(__DIR__) . "/vendor/autoload.php";
+
+// load EC private key
+$ec_priv_jwk = ECPrivateKeyJWK::fromPEM(
+	PEM::fromFile(dirname(__DIR__) . "/test/assets/ec/private_key_P-256.pem"));
+// initialize ES256 signature algorithm and set key ID
+$sig_algo = ES256Algorithm::fromPrivateKey($ec_priv_jwk)->withKeyID("sig-key");
+// initialize A128GCMKW key management algorithm and set key ID
+$kek = "0123456789abcdef";
+$key_algo = A128GCMKWAlgorithm::fromKey($kek)->withKeyID("enc-key");
+// initialize A128CBC-HS256 content encryption algorithm
+$enc_algo = new A128CBCHS256Algorithm();
+// compose claims set
+$claims = new Claims(new IssuerClaim("joe"), 
+	ExpirationTimeClaim::fromString("now + 1 hour"), 
+	new Claim("http://example.com/is_root", true));
+// sign claims to produce JWT as a JWS
+$jwt = JWT::signedFromClaims($claims, $sig_algo);
+// encrypt JWS further to produce JWT as a JWE
+$jwt = $jwt->encryptNested($key_algo, $enc_algo);
+echo "$jwt\n";
